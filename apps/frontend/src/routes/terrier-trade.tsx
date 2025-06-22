6ab0fc936d8596a15853bc586e7dcbed527bb7a5
AutumnQiu99 <autumn.t.qiu@gmail.com>
2025-06-22 11:04:42 -0400

    feat: create terrier trade page using prize card component

--- apps/frontend/src/routes/terrier-trade.tsx
+++ apps/frontend/src/routes/terrier-trade.tsx
@@ -1,13 +1,38 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { PrizeCard } from "../components/prize-card";
 
 export const Route = createFileRoute("/terrier-trade")({
     component: TerrierTrade,
 });
 
 function TerrierTrade() {
+    const prizes = [
+        {
+            name: "Terrier Plushie",
+            cost: 100,
+            claimed: 1,
+            allowedToClaim: 1,
+            remaining: 3,
+            total: 10,
+            imageUrl:
+                "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?auto=format&fit=facearea&w=256&h=256&q=80",
+        },
+        {
+            name: "Scotty Mug",
+            cost: 50,
+            claimed: 0,
+            allowedToClaim: 2,
+            remaining: 5,
+            total: 7,
+            imageUrl:
+                "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?auto=format&fit=facearea&w=256&h=256&q=80",
+        },
+    ];
     return (
-        <div>
-            <p>Hello terrier trade!</p>
+        <div className="p-8 max-w-xl mx-auto flex flex-col gap-6">
+            {prizes.map((prize) => (
+                <PrizeCard key={prize.name} prize={prize} />
+            ))}
         </div>
     );
 }
